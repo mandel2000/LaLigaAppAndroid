26c34d49b362b95d8c342c4122354b5e31d019cf
Manuel Delgado Sánchez <mandelsan2000@gmail.com>
2024-06-25 23:05:17 +0200

    Now retrieving Teams info in teams tab

--- app/src/main/java/es/upsa/mimo/android/laligaapp/ui/teams/TeamsFragment.kt
+++ app/src/main/java/es/upsa/mimo/android/laligaapp/ui/teams/TeamsFragment.kt
@@ -1,7 +1,49 @@
 package es.upsa.mimo.android.laligaapp.ui.teams
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import es.upsa.mimo.android.laligaapp.R
+import es.upsa.mimo.android.laligaapp.network.ApiClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
 class TeamsFragment : Fragment(R.layout.fragment_teams){
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_teams, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Call your ApiService within a coroutine scope
+        lifecycleScope.launch {
+            val response = ApiClient.apiService.getTeams(league = 140, season = 2023)
+
+            if (response.isSuccessful) {
+                val teamData = response.body()
+                teamData?.response?.forEach { resp -> Log.d("TeamsFragment", "Team data: ${resp.team}")}
+
+            } else {
+                // Handle API error
+                Log.e("TeamsFragment", "Error: ${response.message()}")
+            }
+
+
+        }
+    }
+
+
 
 }
